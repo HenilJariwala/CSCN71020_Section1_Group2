0b3f9ed7334014dcefb7559e1d6236b115250feb
Rishi Umaria <rishiumaria@gmail.com>
2023-11-18 12:03:43 -0500

    Updated Triangle Unit test

--- UnitTest_Rectangle/UnitTest_Triangle.cpp
+++ UnitTest_Rectangle/UnitTest_Triangle.cpp
@@ -1,2 +1,40 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "..//PolygonChecker/main.c"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+TEST_CLASS(UnitTest1)
+{
+public:
+
+    TEST_METHOD(TestTriangleAnalysis)
+    {
+        // Arrange
+        int side1 = 3;
+        int side2 = 4;
+        int side3 = 5;
+
+        // Act
+        char* result = analyzeTriangle(side1, side2, side3);
+
+        // Assert
+        Assert::AreEqual("Scalene triangle", result);
+    }
+
+    TEST_METHOD(TestCheckForTriangle)
+    {
+        // Arrange
+        int side1 = 3;
+        int side2 = 4;
+        int side3 = 5;
+
+        // Act
+        bool isValid = checkForTriangle(side1, side2, side3);
+
+        // Assert
+        Assert::IsTrue(isValid);
+    }
+
+    // Add more tests as needed
+};
