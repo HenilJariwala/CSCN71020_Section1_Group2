e972a42778edeb9a8c8b667b25353001396f33ee
Krishi Nikeshkumar Patel <Kpatel0936@conestogac.on.ca>
2023-11-18 09:03:50 -0500

    calculatedistane testcases

--- UnitTest_Rectangle/UnitTest_Rectangle.cpp
+++ UnitTest_Rectangle/UnitTest_Rectangle.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
+extern "C" float calculateDistance(int x1, int y1, int x2, int y2);
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTestRectangle
@@ -9,8 +11,65 @@ namespace UnitTestRectangle
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(calculateDistance01)
 		{
+			int input_x1 = 0;
+			int input_y1 = 0;
+			int input_x2 = 3;
+			int input_y2 = 4;
+			float actual_distance = calculateDistance(input_x1,input_y1,input_x2,input_y2);
+			float expected = 5;
+			Assert::AreEqual(expected, actual_distance);
+		}
+		TEST_METHOD(calculateDistance02)
+		{
+			int input_x1 = 1;
+			int input_y1 = 2;
+			int input_x2 = 4;
+			int input_y2 = 6;
+			float actual_distance = calculateDistance(input_x1, input_y1, input_x2, input_y2);
+			float expected = 5;
+			Assert::AreEqual(expected, actual_distance);
+		}
+		TEST_METHOD(calculateDistance03)
+		{
+			int input_x1 = 1;
+			int input_y1 = 1;
+			int input_x2 = 1;
+			int input_y2 = 1;
+			float actual_distance = calculateDistance(input_x1, input_y1, input_x2, input_y2);
+			float expected = 0;
+			Assert::AreEqual(expected, actual_distance);
+		}
+		TEST_METHOD(calculateDistance03)
+		{
+			int input_x1 = -1;
+			int input_y1 = -1;
+			int input_x2 = -1;
+			int input_y2 = -1;
+			float actual_distance = calculateDistance(input_x1, input_y1, input_x2, input_y2);
+			float expected = 0;
+			Assert::AreEqual(expected, actual_distance);
+		}
+		TEST_METHOD(calculateDistance04)
+		{
+			int input_x1 = -2;
+			int input_y1 = -3;
+			int input_x2 = -4;
+			int input_y2 = -8;
+			float actual_distance = calculateDistance(input_x1, input_y1, input_x2, input_y2);
+			float expected = 5.39;
+			Assert::AreEqual(expected, actual_distance);
+		}
+		TEST_METHOD(calculateDistance05)
+		{
+			int input_x1 = -2;
+			int input_y1 = -3;
+			int input_x2 = -4;
+			int input_y2 = -8;
+			float actual_distance = calculateDistance(input_x1, input_y1, input_x2, input_y2);
+			float expected = 5.39;
+			Assert::AreEqual(expected, actual_distance);
 		}
 	};
 }
---
+++ triangle_UnitTest/triangle_UnitTest.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace triangleUnitTest
+{
+	TEST_CLASS(triangleUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+}
